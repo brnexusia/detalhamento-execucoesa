6d7d8cddc93eca24cfc6a35bad5f519d317cd4b7
brnexusia <brnexusia@gmail.com>
2026-08-22 07:27:08 -0300

    ui: enable fluid canvas mode

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './saas.css'
+import './canvas.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
