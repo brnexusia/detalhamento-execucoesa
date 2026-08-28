306f3e63a9fc11bc85f7ab4841b12bfc1d675e83
brnexusia <brnexusia@gmail.com>
2026-08-28 00:05:16 -0300

    feat(scanner): mount module 5 publish controls

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import ScannerPublishModule from './ScannerPublishModule'
 import './styles.css'
 import './saas.css'
 import './canvas.css'
@@ -12,5 +13,6 @@ import './yellow-ux-fixes.css'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <ScannerPublishModule />
   </StrictMode>,
 )
