bf401023249acfc72814b3dee802d9cc3cd2d190
brnexusia <brnexusia@gmail.com>
2026-08-28 19:34:36 -0300

    Demo: montar botão voltar na loja demo

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import DemoBackButton from './DemoBackButton'
 import ScannerPublishModule from './ScannerPublishModule'
 import './styles.css'
 import './saas.css'
@@ -9,10 +10,12 @@ import './platform-admin.css'
 import './ux-polish.css'
 import './feed-arrow-fix.css'
 import './yellow-ux-fixes.css'
+import './demo-back-button.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <DemoBackButton />
     <ScannerPublishModule />
   </StrictMode>,
 )
