662a2f1aa968cd596c1ccab1630b9995b59a8452
brnexusia <brnexusia@gmail.com>
2026-08-29 13:48:07 -0300

    perf: aplicar otimizações visuais na vitrine

--- src/PublicRoute.tsx
+++ src/PublicRoute.tsx
@@ -2,6 +2,7 @@ import PublicStore from './PublicStore'
 import DemoBackButton from './DemoBackButton'
 import './feed-arrow-fix.css'
 import './demo-back-button.css'
+import './public-performance.css'
 
 export default function PublicRoute() {
   return <>
