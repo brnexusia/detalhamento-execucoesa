83247991db7043e5426e34a565e53ef32ad18850
brnexusia <brnexusia@gmail.com>
2026-08-29 13:46:47 -0300

    perf: reduzir bundle inicial global

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
-import DemoBackButton from './DemoBackButton'
-import ScannerPublishModule from './ScannerPublishModule'
 import './styles.css'
 import './saas.css'
 import './canvas.css'
-import './platform-admin.css'
-import './ux-polish.css'
-import './feed-arrow-fix.css'
-import './yellow-ux-fixes.css'
-import './demo-back-button.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
-    <DemoBackButton />
-    <ScannerPublishModule />
   </StrictMode>,
 )
