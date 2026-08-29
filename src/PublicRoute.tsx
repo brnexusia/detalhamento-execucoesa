2fea29eca55d4ad7e07e7ac26de0528f77e1e32b
brnexusia <brnexusia@gmail.com>
2026-08-29 13:48:38 -0300

    perf: ativar prioridade de mídia na vitrine

--- src/PublicRoute.tsx
+++ src/PublicRoute.tsx
@@ -1,11 +1,13 @@
 import PublicStore from './PublicStore'
 import DemoBackButton from './DemoBackButton'
+import PublicPerformanceRuntime from './PublicPerformanceRuntime'
 import './feed-arrow-fix.css'
 import './demo-back-button.css'
 import './public-performance.css'
 
 export default function PublicRoute() {
   return <>
+    <PublicPerformanceRuntime />
     <PublicStore />
     <DemoBackButton />
   </>
