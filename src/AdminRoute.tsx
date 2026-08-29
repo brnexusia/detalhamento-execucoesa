eaaa776674484bc13dfe95534ec31b9ade4f3a56
brnexusia <brnexusia@gmail.com>
2026-08-29 15:13:41 -0300

    m2: adicionar acesso ao modulo de estoque

--- src/AdminRoute.tsx
+++ src/AdminRoute.tsx
@@ -1,13 +1,22 @@
 import AdminApp from './AdminApp'
 import ScannerModule1 from './ScannerModule1'
 import ScannerPublishModule from './ScannerPublishModule'
+import BusinessFeaturesPanel from './BusinessFeaturesPanel'
 import './ux-polish.css'
 import './yellow-ux-fixes.css'
 
+function go(path: string) {
+  window.history.pushState({}, '', path)
+  window.dispatchEvent(new PopStateEvent('popstate'))
+}
+
 export default function AdminRoute() {
+  const features = window.location.pathname === '/painel/recursos' || window.location.pathname.startsWith('/painel/recursos/')
+  if (features) return <BusinessFeaturesPanel />
   return <>
     <AdminApp />
     <ScannerModule1 />
     <ScannerPublishModule />
+    <button className="business-launcher" type="button" onClick={() => go('/painel/recursos')}>Estoque e recursos</button>
   </>
 }
