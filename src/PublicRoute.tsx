ea307d448d1cf9ef269f78f0345c58c673485c66
brnexusia <brnexusia@gmail.com>
2026-08-29 15:00:50 -0300

    m1: ativar loja com galeria completa

--- src/PublicRoute.tsx
+++ src/PublicRoute.tsx
@@ -1,14 +1,15 @@
-import PublicStore from './PublicStore'
+import PublicStoreV2 from './PublicStoreV2'
 import DemoBackButton from './DemoBackButton'
 import PublicPerformanceRuntime from './PublicPerformanceRuntime'
 import './feed-arrow-fix.css'
 import './demo-back-button.css'
 import './public-performance.css'
+import './product-gallery.css'
 
 export default function PublicRoute() {
   return <>
+    <PublicStoreV2 />
     <PublicPerformanceRuntime />
-    <PublicStore />
     <DemoBackButton />
   </>
 }
